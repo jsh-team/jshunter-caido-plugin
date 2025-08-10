e338579133b7b3ec4b9999f0b32045a1bcbb8451
Rollinx1 <cmvargas66@gmail.com>
2025-08-09 21:08:20 -0400

    Latest Changes

--- caido.config.ts
+++ caido.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   id,
   name: "JSHunter",
   description: "Analyze all your JavaScript in one place",
-  version: "0.0.0",
+  version: "1.0.0",
   author: {
     name: "Caido Labs Inc.",
     email: "dev@caido.io",
